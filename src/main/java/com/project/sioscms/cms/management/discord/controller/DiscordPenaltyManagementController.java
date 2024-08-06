72f277e3d07a4ee57f463e5fd683e416c2a0e51e
Sioria-Yu <chfur12@gmail.com>
2024-08-06 14:18:26 +0900

    1. 디스코드 패널티 관리용 컨트롤러 추가

---
+++ src/main/java/com/project/sioscms/cms/management/discord/controller/DiscordPenaltyManagementController.java
@@ -0,0 +1,15 @@
+package com.project.sioscms.cms.management.discord.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/cms/discord/penalty")
+public class DiscordPenaltyManagementController {
+
+
+}
